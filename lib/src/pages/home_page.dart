fe8529dc5c5b3a7c74820d20b2439afa1fb10625
SM Shohag <fsm.cts@gmail.com>
2023-12-17 11:23:58 +0600

    fixed

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -9,14 +9,69 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          mlController.authenticate();
+        },
+        child: Icon(
+          Icons.face_4_rounded,
+          color: Colors.white,
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.add),
+        child: Center(
+          child: Obx(
+            () => Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        mlController.pickImage(true);
+                      },
+                      icon: Icon(Icons.add),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        mlController.pickImage(false);
+                      },
+                      icon: Icon(Icons.add),
+                    ),
+                  ],
+                ),
+                if (mlController.img1.value != null)
+                  Container(
+                    width: 150,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(
+                        22,
+                      ),
+                      child: Image.memory(mlController.img1.value!),
+                    ),
+                  ),
+                SizedBox(
+                  height: 12,
+                ),
+                if (mlController.img2.value != null)
+                  Container(
+                    width: 150,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(
+                        22,
+                      ),
+                      child: Image.memory(mlController.img2.value!),
+                    ),
+                  ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
